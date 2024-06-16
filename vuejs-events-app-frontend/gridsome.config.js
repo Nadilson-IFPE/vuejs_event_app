fd43a3f4be22a24a95d5f98bf3838c4e0dd4b336
Nadilson <nadilson@protonmail.com>
2024-06-15 23:36:27 -0300

    Gridsome setup

---
+++ vuejs-events-app-frontend/gridsome.config.js
@@ -0,0 +1,12 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'VueJS Events App',
+  siteDescription: `Traversy Media's VueJS-Gridsome-Strapi tutorial to post or find online events`,
+  siteUrl: 'https://www.youtube.com/watch?v=vB6rmWCmANA',
+  plugins: []
+}
