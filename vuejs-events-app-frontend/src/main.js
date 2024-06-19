2ffee3b8b2c57841d3e7441ec5414f49b7b4ff3a
Nadilson <nadilson@protonmail.com>
2024-06-19 19:03:29 -0300

    Vuetify setup

--- vuejs-events-app-frontend/src/main.js
+++ vuejs-events-app-frontend/src/main.js
@@ -1,9 +1,26 @@
-// This is the main.js file. Import global CSS and scripts here.
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
-
+// Several CSP or CORS errors on Vuetify versions starting 3.6.9. 
+// Downgrade to version 2.3.6 to avoid site rendering errors.
+// See package.json
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 import DefaultLayout from '~/layouts/Default.vue'
 
-export default function (Vue, { router, head, isClient }) {
+export default function (Vue, { appOptions, head }) {
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css',
+  })
+  
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900',
+  });
+  
+  const opts = {}
+  Vue.use(Vuetify)
+  
+  appOptions.vuetify = new Vuetify(opts);
+  
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
