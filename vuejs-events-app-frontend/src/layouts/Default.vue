527192bf700e2685a35c0b05e194f205868c6ecb
Nadilson <nadilson@protonmail.com>
2024-06-20 00:57:13 -0300

    Top navigation

--- vuejs-events-app-frontend/src/layouts/Default.vue
+++ vuejs-events-app-frontend/src/layouts/Default.vue
@@ -1,8 +1,31 @@
 <template>
   <v-app>
+
+    <v-app-bar app>
+      <v-toolbar-title>
+        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+      </v-toolbar-title>
+
+      <v-text-field 
+        v-model="searchText" 
+        @click:clear="searchText = ''" 
+        placeholder="Search" 
+        class="ml-8"
+        style="max-width: 350px;" 
+        prepend-inner-icon="mdi-magnify" 
+        clearable 
+        outlined 
+        dense 
+        hide-details />
+
+      <v-spacer />
+      <v-btn>Create Event</v-btn>
+    </v-app-bar>
+
     <v-main>
       <slot />
     </v-main>
+
   </v-app>
 </template>
 
@@ -15,3 +38,11 @@ query {
   }
 }
 </static-query>
+
+
+<style scoped>
+.v-toolbar__title a {
+  text-decoration: none;
+  color: #000;
+}
+</style>
